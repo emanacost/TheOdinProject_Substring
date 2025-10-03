c908b1098bc381f07263515cedec81a58b893285
eacosta <eacosta0411@gmail.com>
2025-10-02 23:15:08 -0400

    Add main script file with overall plan

---
+++ Substring.rb
@@ -0,0 +1,7 @@
+# Overall plan
+  # Take string
+  # Convert it to an array
+  # Iterate array against "dictionary" array
+  # Each time there is a match, log it to a new hash
+  # New hash will keep count of matches
+  # Return hash
